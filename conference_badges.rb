5952ed9c820be747e8bf3d63a9032d3d34a27aad
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 04:21:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
-  return badge_maker(name_array)
+  for names in name_array do
+  return badge_maker(names)
 end
 
 def assign_rooms(speakers)
