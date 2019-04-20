37213c99b8b30e7221b16f9c6a2bcda80b6d19ea
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 05:46:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(speakers)
 
   speakers.each_with_index{|name, room|
     room_plus_one = room + 1
-    room_assignments.push("Hello #{name}! You'll be assigned to #{room_plus_one}!")}
+    room_assignments.push("Hello #{name}! You'll be assigned to room #{room_plus_one}!")}
 
   return room_assignments
 =begin
